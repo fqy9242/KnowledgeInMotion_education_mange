da773492afbd9e75fb3e38cb494ac36a09be7e3e
qht <qht20050506@outlook.com>
2024-05-10 13:33:08 +0800

    数据库时方面初步完成

--- mysql脚本.sql
+++ mysql脚本.sql
@@ -25,6 +25,13 @@ CREATE TABLE t_student(
 	-- 学生照片 存储数据为url地址
 	student_photograph VARCHAR(100)
 );
+-- 学生表添加初始数据
+INSERT INTO t_student VALUES("2331020130229",3,39,'覃惠通',19,1,'17777402163','qht123','广西梧州',
+'https://pic1.zhimg.com/v2-3810812c3e201f922e8b25460a2f50db_r.jpg');
+
+INSERT INTO t_student VALUES("2331020133333",1,22,'杜维',13,1,'13888882222','zxc2342','贵州六盘水', null);
+
+INSERT INTO t_student VALUES("2331020133133",2, 23, '王大锤', 37, 1, '17999999999','wdc520','菏泽曹县', null);
 -- 查询学生表
 SELECT * FROM t_student;
 
@@ -51,36 +58,60 @@ CREATE TABLE t_teacher(
 	-- 教师照片 存储数据为url地址
 	teacher_photograph VARCHAR(100)
 );
+-- 往学生表添加初始数据
+INSERT INTO t_teacher VALUES(2005333,3, 39, '李白','班主任', 35, 1, '18888888888','gxnzd123','http://pic.wankacn.com/2017-05-19_591f05253b542.jpeg');
+INSERT INTO t_teacher VALUES(2005355,2, 33, '杜甫','班主任', 42, 1, '19999999999', null , null);
+INSERT INTO t_teacher VALUES(2001333,3, null, '冯诺依曼','专任教师', 555, 1, '13333333333','iloveChina',null);
 -- 查询教师表
 SELECT * FROM t_teacher;
 -- 创建学院表
 CREATE TABLE t_college(
 	-- 学院id 主键,自动增长
-	college_id TINYINT PRIMARY KEY auto_increment,
+	college_id TINYINT PRIMARY KEY AUTO_INCREMENT,
 	-- 学院名
-	college_name VARCHAR(16) NOT NULL,
+	college_name VARCHAR(16) NOT NULL
 );
-
--- 创建成绩表
-CREATE TABLE t_score(
-	-- 课程id，主键
-	course_id int PRIMARY KEY NOT NULL,
-	-- 学生id 主键
-	student_id VARCHAR(30) PRIMARY KEY NOT NULL,
-	-- 课程名
-	course_name VARCHAR(16),
-	-- 成绩
-	course_score DECIMAL,
-);
+-- 往学院表添加初始数据
+INSERT INTO t_college VALUES(null,'麻省理工学院');
+INSERT INTO t_college VALUES(null,'文学院');
+INSERT INTO t_college VALUES(null,'信息工程学院');
+-- 查询学院表
+SELECT * FROM t_college;
 
 -- 创建课程表
 CREATE TABLE t_course(
 	-- 课程id 主键 自动增长
-	course_id PRIMARY KEY auto_increment,
+	course_id INT PRIMARY KEY AUTO_INCREMENT,
 	-- 课程名
 	course_name VARCHAR(16) NOT NULL,
 	-- 开课学院id
-	course_college_id TINYINT,
+	course_college_id TINYINT
 );
+-- 往课程表插入初始数据
+INSERT INTO t_course VALUES(null, '小学生精通java',3);
+INSERT INTO t_course VALUES(null, '前台与后厨基础',3);
+INSERT INTO t_course VALUES(null, '小学语文',2);
+-- 查询课程表
+SELECT * FROM t_course;
+
+-- 创建成绩表
+CREATE TABLE t_score(
+	-- 课程id，主键
+	course_id INT NOT NULL,
+	-- 学生id 主键
+	student_id VARCHAR(30) NOT NULL,
+	-- 课程名
+	course_name VARCHAR(16),
+	-- 成绩
+	course_score DECIMAL,
+	-- 联合主键
+	PRIMARY KEY (course_id, student_id)
+);
+-- 往成绩表插入数据
+INSERT INTO t_score VALUES(1, '2331020130229', '小学生精通java', 99);
+INSERT INTO t_score VALUES(2, '2331020130229', '前台与后厨基础', 99);
+INSERT INTO t_score VALUES(3, '2331020130229', '小学语文', 99);
+-- 查询成绩表
+SELECT * FROM t_score;
 
 
